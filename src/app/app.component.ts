d6ad7cb55eae04616c8891fc43cbce5f18da69c7
FrankTarco <jhoanangelo123.tc@gmail.com>
2023-11-11 22:38:17 -0500

    PruebaDeploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Semana01-prueba';
+  title = 'Franklin Tarco Pariapaza';
 }
